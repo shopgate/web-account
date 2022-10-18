e1852ab63a38eff7b5a780d1fabfbf9b76f23e87
Konstantin K <k@componentk.com>
2022-10-18 21:28:39 +0300

    SG-157 adjusting replace to use regex expressions

--- frontend/components/AccountItem/actions.js
+++ frontend/components/AccountItem/actions.js
@@ -1,6 +1,6 @@
-import { FETCH_CHECKOUT_URL_TIMEOUT, fetchCheckoutUrl } from '@shopgate/engage/checkout'
-import { historyPush, logger } from '@shopgate/engage/core'
-import getConfig from '../../helpers/getConfig'
+import { historyPush, logger } from '@shopgate/engage/core';
+import { FETCH_CHECKOUT_URL_TIMEOUT, fetchCheckoutUrl } from '@shopgate/engage/checkout';
+import getConfig from '../../helpers/getConfig';
 
 /**
  * Fetches Shopware checkout URL changes it to the URL of account page and redirects to that page.
@@ -8,17 +8,17 @@ import getConfig from '../../helpers/getConfig'
  * @return {null}
  */
 export const openShopwareAccountPage = replacement => (dispatch) => {
-  const started = Date.now()
+  const started = Date.now();
   dispatch(fetchCheckoutUrl())
     .then((url) => {
       // Forget it if it took more than PWA allows. User is already back.
       if (Date.now() - started > FETCH_CHECKOUT_URL_TIMEOUT) {
-        return
+        return;
       }
-      const { urlSlugToReplace } = getConfig()
-      const linkUrl = replacement ? url.replace(new RegExp(urlSlugToReplace, 'ig'), replacement) : url
+      const { urlSlugToReplace } = getConfig();
+      const linkUrl = replacement ? url.replace(new RegExp(urlSlugToReplace, 'ig'), replacement) : url;
       // Open the link
-      dispatch(historyPush({ pathname: linkUrl }))
+      dispatch(historyPush({ pathname: linkUrl }));
     })
     .catch(error => logger.error('Error fetching Shopware Account Page URL', error));
 };
